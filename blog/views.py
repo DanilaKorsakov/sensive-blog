72136ecf233d0d5247e0d2fd557ed6e6e4ea2da2
DanilaKorsakov <109290153+DanilaKorsakov@users.noreply.github.com>
2024-12-18 16:10:28 +0300

    reduce db queries number

--- blog/views.py
+++ blog/views.py
@@ -28,14 +28,14 @@ def index(request):
 
     most_popular_posts = (
         Post.objects.popular()
-        .prefetch_related("author")[:5]
+        .prefetch_related("author", "tags")[:5]
         .fetch_with_comments_count()
     )
 
     fresh_posts = (
         Post.objects.annotate(comments_count=Count('comments'))
         .order_by('-published_at')
-        .prefetch_related('author')
+        .prefetch_related('author', "tags")[:5]
     )
     most_fresh_posts = list(fresh_posts)[-5:]
 
