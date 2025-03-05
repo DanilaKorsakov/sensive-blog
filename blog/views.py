8abf1f290c7fa5e3ed224d3ba5fabc3f002269cf
BasicTemplates <kvashnindan@gmail.com>
2025-03-05 13:50:51 +0300

    optimize comment count, but have bad timing

--- blog/views.py
+++ blog/views.py
@@ -16,6 +16,18 @@ def serialize_post(post):
         'first_tag_title': post.tags.all()[0].title,
     }
 
+def serialize_post_optimized(post):
+    return {
+        'title': post.title,
+        'teaser_text': post.text[:200],
+        'author': post.author.username,
+        'comments_amount': post.comments_amount,
+        'image_url': post.image.url if post.image else None,
+        'published_at': post.published_at,
+        'slug': post.slug,
+        'tags': [serialize_tag(tag) for tag in post.tags.all()],
+        'first_tag_title': post.tags.all()[0].title,
+    }
 
 def serialize_tag(tag):
     return {
@@ -28,9 +40,9 @@ def index(request):
 
     posts = Post.objects.all()
 
-    most_popular_posts = Post.objects.annotate(likes_count = Count('likes')).order_by('-likes_count').prefetch_related('author')[:5]
+    most_popular_posts = Post.objects.annotate(comments_amount=Count('comments'),likes_count = Count('likes', distinct=True)).order_by('-likes_count').prefetch_related('author')[:5]
 
-    fresh_posts = Post.objects.order_by('published_at').prefetch_related('author')
+    fresh_posts = Post.objects.annotate(comments_amount = Count('comments')).order_by('published_at').prefetch_related('author')
     most_fresh_posts = list(fresh_posts)[-5:]
 
     tags = Tag.objects.all()
@@ -38,9 +50,9 @@ def index(request):
 
     context = {
         'most_popular_posts': [
-            serialize_post(post) for post in most_popular_posts
+            serialize_post_optimized(post) for post in most_popular_posts
         ],
-        'page_posts': [serialize_post(post) for post in most_fresh_posts],
+        'page_posts': [serialize_post_optimized(post) for post in most_fresh_posts],
         'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
     }
     return render(request, 'index.html', context)
