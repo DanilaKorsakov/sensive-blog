266461f81da8268ac099b23d92a3d107b212be68
BasicTemplates <kvashnindan@gmail.com>
2025-06-24 16:56:09 +0300

    deleted useless code

--- blog/views.py
+++ blog/views.py
@@ -16,30 +16,11 @@ def serialize_post(post):
         'first_tag_title': post.tags.all()[0].title,
     }
 
-def serialize_not_optimized(post):
-    return {
-        'title': post.title,
-        'teaser_text': post.text[:200],
-        'author': post.author.username,
-        'comments_amount': post.comments_count,
-        'image_url': post.image.url if post.image else None,
-        'published_at': post.published_at,
-        'slug': post.slug,
-        'tags': [serialize_tag_not_optimized(tag) for tag in post.tags.all()],
-        'first_tag_title': post.tags.all()[0].title,
-    }
-
 
 def serialize_tag(tag):
     return {
         'title': tag.title,
         'posts_with_tag': tag.posts_count,
-    }
-
-def serialize_tag_not_optimized(tag):
-    return {
-        'title': tag.title,
-        'posts_with_tag': len(Post.objects.filter(tags=tag)),
     }
 
 
